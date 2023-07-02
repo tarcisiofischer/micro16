816e42d1ee398ae5a49075f3d201856c31621ea1
Tarcisio Fischer <tarcisio.fischer@codethink.co.uk>
2023-07-02 12:02:42 +0100

    Use cstdint structures instead of unsigned int/char on specs.h

--- src/specs.h
+++ src/specs.h
@@ -1,15 +1,13 @@
 #ifndef MICRO16_SPECS_H
 #define MICRO16_SPECS_H
 
-// TODO: Add more suitable data structures for those. I tried std::bitset, but it uses a `long int` for storage, so
-//       for small bitsets it is not "memory friendly" (Although it probably make sense for memory alignment?).
-//       But would be better to use it from the typing perspective (Today, a Byte and a Nibble are "the same thing").
-using Register = unsigned int;
-using Byte = unsigned char;
-using Nibble = unsigned char; // Only 4bits used
-using Word = unsigned int;
-using Address = unsigned int;
-using Instruction = unsigned int;
+#include <cstdint>
+
+using Register = uint16_t;
+using Byte = uint8_t;
+using Nibble = uint8_t; // Only 4bits used
+using Address = uint16_t;
+using Instruction = uint16_t;
 
 static constexpr auto CODE_BANK = 0;
 static constexpr auto MMIO_BANK = 1;
